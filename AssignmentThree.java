f1185eb30cc718f9a49a29987624d94f09d7c5e6
Manmohan Shaw <shawmanmohan@gmail.com>
2016-06-22 10:26:27 +0530

    Updated

--- AssignmentThree.java
+++ AssignmentThree.java
@@ -1,5 +1,8 @@
 package com.sessionone;
 
 public class AssignmentThree {
-
+pubiic static void main(String args[])
+{
+System.out.println("The String and Boolean cannot be typecast into INT");
+}
 }
