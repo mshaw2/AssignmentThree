bd74d93d5b1c011d80d8c1bde897036b1dfbdcc6
Manmohan Shaw <shawmanmohan@gmail.com>
2016-06-23 19:48:50 +0530

    Session2:AssignmentThree

--- AssignmentThree.java
+++ AssignmentThree.java
@@ -1,8 +1,22 @@
-package com.sessionone;
+package com.sessiontwo;
 
 public class AssignmentThree {
-pubiic static void main(String args[])
-{
-System.out.println("The String and Boolean cannot be typecast into INT");
+
+	public static void main(String[] args) {
+		for (int i=1; i<=5; i++){
+			for (int j=1; j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		for(int i=5;i>=0;i--) { 
+			for(int j=1; j<i; j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+	}	
+	}
 }
-}
+
